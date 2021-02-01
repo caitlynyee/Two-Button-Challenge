c322aecbad2504171c33eb3060de2c685c5dda82
Caitlyn Yee <yeecf@bc.edu>
2021-02-01 00:00:35 -0500

    Completed Text Alignment Challenge

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -19,11 +19,13 @@ class ViewController: UIViewController {
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
         messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
     }
     
     @IBAction func anotherMessageButtonPressed(_ sender: UIButton) {
         messageLabel.text = "You Are Great!"
         messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
     }
 }
 
